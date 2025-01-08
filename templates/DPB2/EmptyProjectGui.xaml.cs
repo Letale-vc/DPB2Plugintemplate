f8eac840d1f09aaf6db0470f6d71337da5c735fa
Helpless <letalerv@gmail.com>
2025-01-08 07:08:22 +0200

    initial

---
+++ templates/DPB2/EmptyProjectGui.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+
+namespace EmptyProject
+{
+	/// <summary>
+	/// Interaction logic for EmptyProjectGui.xaml
+	/// </summary>
+	public partial class EmptyProjectGui
+	{
+		public EmptyProjectGui()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			EmptyProjectSettings.Instance.SettingNumber++;
+		}
+	}
+}
---
+++ templates/DPB2/EmptyProjectSettings.cs
@@ -0,0 +1,30 @@
+﻿using DreamPoeBot.Loki;
+using DreamPoeBot.Loki.Common;
+using System.ComponentModel;
+
+namespace EmptyProject
+{
+	internal class EmptyProjectSettings : JsonSettings
+	{
+		private static EmptyProjectSettings _instance;
+		public static EmptyProjectSettings Instance => _instance ??= new EmptyProjectSettings();
+
+		private EmptyProjectSettings()
+			: base(GetSettingsFilePath(Configuration.Instance.Name, $"{nameof(EmptyProjectSettings)}.json"))
+		{
+		}
+
+		private int _settingNumber;
+
+		[DefaultValue(256)]
+		public int SettingNumber
+		{
+			get => _settingNumber;
+			set
+			{
+				_settingNumber = value;
+				NotifyPropertyChanged(() => SettingNumber);
+			}
+		}
+	}
+}
